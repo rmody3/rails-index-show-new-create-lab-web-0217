b2d63b9a92d7dc74e5c0d960fa7f38469250da0e
rmody3 <rmody3@gmail.com>
2017-03-01 13:50:40 -0500

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,58 @@
+class CouponsController < ApplicationController
+  before_action :set_coupon, only: :show
+
+  # GET /coupons
+  # GET /coupons.json
+  def index
+    @coupons = Coupon.all
+  end
+
+  # GET /coupons/1
+  # GET /coupons/1.json
+  def show
+  end
+
+  # GET /coupons/new
+  def new
+    @coupon = Coupon.new
+  end
+
+  # POST /coupons
+  # POST /coupons.json
+  def create
+    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to coupon_path(@coupon)
+
+  end
+
+  # PATCH/PUT /coupons/1
+  # PATCH/PUT /coupons/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @coupon.update(coupon_params)
+  #       format.html { redirect_to @coupon, notice: 'Coupon was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @coupon }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @coupon.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+  #
+  # # DELETE /coupons/1
+  # # DELETE /coupons/1.json
+  # def destroy
+  #   @coupon.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to coupons_url, notice: 'Coupon was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_coupon
+      @coupon = Coupon.find(params[:id])
+    end
+
+end
